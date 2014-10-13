e506c12573bb5b8965395b8a8ee7169824977638
biwin <biwin007@gmail.com>
2014-10-13 18:15:40 +0530

    slug help text and meta ordering based on asc title

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,11 @@ from django.db import models
 
 class Category(models.Model):
 	title = models.CharField(max_length=250, help_text='Max 250 chars.')
-	slug = models.SlugField(unique=True)
+	slug = models.SlugField(unique=True, help_text='Generated automatically from the title, needs to be unique')
 	description = models.TextField()
 
 	class Meta:
+		ordering = ['title']
 		verbose_name_plural = 'Categories'
 
 	def __unicode__(self):
