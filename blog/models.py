934ef769efd6770ebfa53ba59db25eb7f8a77581
biwin <biwin007@gmail.com>
2014-10-14 20:20:31 +0530

    markdown

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import datetime
 from django.contrib.auth.models import User
-# Create your models here.
+from markdown import markdown
 
 
 class Category(models.Model):
@@ -39,7 +39,21 @@ class Entry(models.Model):
 	featured = models.BooleanField(default=False)
 	status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS)
 	categories = models.ManyToManyField(Category)
+	excerpt_html = models.TextField(editable=False, blank=True)
+	body_html = models.TextField(editable=False, blank=True)
 
 	class Meta:
 		verbose_name_plural = 'Entries'
+		ordering = ['-pub_date']
 
+	def __unicode__(self):
+		return self.title
+
+	def save(self, force_insert=False, force_update=False):
+		self.body_html = markdown(self.body)
+		if self.excerpt:
+			self.excerpt_html = markdown(self.excerpt)
+		super(Entry, self).save(force_insert, force_update)
+
+	def get_absoulte_url(self):
+		return '/weblog/%s/%s/' % (self.pub_date.strftime("%Y/%b/%d").lower(), self.slug)
