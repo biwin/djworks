8b9c3db4e310de782f2b2e3619c227ff04b83bfa
biwin <biwin007@gmail.com>
2014-10-14 20:20:57 +0530

    adds the views for entry list

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from blog.models import Entry
+
+
+def entries_index(request):
+	return render_to_response('blog/index.html', {'entry_list': Entry.objects.all()})
