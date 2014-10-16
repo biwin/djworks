f62f5e894c27a31f2e8ce8286d2b572c4b9e64a2
biwin <biwin007@gmail.com>
2014-10-16 22:54:26 +0530

    change the template name

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from blog.models import Entry
 
 
 def entries_index(request):
-	return render_to_response('blog/index.html', {'entry_list': Entry.objects.all()})
+	return render_to_response('blog/entry_index.html', {'entry_list': Entry.objects.all()})
